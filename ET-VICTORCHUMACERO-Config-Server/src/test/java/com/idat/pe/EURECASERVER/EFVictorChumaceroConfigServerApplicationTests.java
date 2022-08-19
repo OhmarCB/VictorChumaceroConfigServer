60dfd00bf6cb92ed6b875613074c4fae8b8f0e5e
OhmarCB <104176599+OhmarCB@users.noreply.github.com>
2022-08-18 23:33:14 -0500

    Add files via upload

---
+++ ET-VICTORCHUMACERO-Config-Server/src/test/java/com/idat/pe/EURECASERVER/EFVictorChumaceroConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.pe.EURECASERVER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFVictorChumaceroConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
